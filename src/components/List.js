a0e52f9cf14e31af1cec4d65f7d6cd572de66876
Felix <35155993+FelixPogosyan2001@users.noreply.github.com>
2019-06-29 22:16:42 +0300

    Update List.js

--- src/components/List.js
+++ src/components/List.js
@@ -1,25 +1,17 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {Change_Color} from '../store/actions.js';
 
-export class List extends React.Component {
-    state = {
-        done : false
-    }
+class List extends React.Component {   
     handleClick = () => {
-        this.setState({
-            done : !this.state.done
-        });         
-    }
-    componentDidUpdate() {
-        if (this.state.done) {
-            this.flag.classList.add('backgr');
-            this.tag.className =  'galD fas fa-check';
-        } else {
-            this.flag.classList.remove('backgr');
-            this.tag.className =  'gal fas fa-check';
-        }
+        var param = this.props.done;
+        this.props.onhandleChange();
+        this.props.Change_Color(!param,this.props.id); 
     }
     drag_start = (e) => {
         e.dataTransfer.setData('content',e.target.textContent);
+        localStorage.removeItem(this.props.id)
         setTimeout(()=>{this.flag.className = 'hide';},0);
         if(!localStorage.getItem(this.props.name)) {
             return false;
@@ -34,12 +26,30 @@ export class List extends React.Component {
     }
     
     render() {
+        var value = null;
+        for (let key in this.props.colorOfStore) {
+            if (this.props.id == key) {
+                value = this.props.colorOfStore[key];
+                localStorage.setItem(key,JSON.stringify(value));
+            }      
+        }
         return(
-            <li draggable = 'true' onDragStart = {this.drag_start} ref = {(node) => this.flag = node} className = 'list'>
+            <li draggable = 'true' onDragStart = {this.drag_start} ref = {(node) => this.flag = node} className = {JSON.parse(localStorage.getItem(this.props.id)) ? 'backgr list' : 'list'}>
                   {this.props.text}
-                  <i onClick = {this.handleClick} ref = {(node) => this.tag = node} className ="gal fas fa-check">
-                  {this.state.done && <i className = 'fas fa-slash' id = 'text_of_li'></i>} </i>
+                  <i onClick = {this.handleClick} ref = {(node) => this.tag = node} className = {JSON.parse(localStorage.getItem(this.props.id)) ? 'galD fas fa-check' : 'gal fas fa-check'}>
+                  {JSON.parse(localStorage.getItem(this.props.id)) && <i className = 'fas fa-slash' id = 'text_of_li'></i>} </i>
              </li>
         )
     }
 }
+const mapStateToProps = (state) => {
+    return {
+        colorOfStore : state.color
+    }
+}
+const mapActionsToProps = (dispatch) => {
+    return {
+        Change_Color : bindActionCreators(Change_Color,dispatch)
+    }
+}
+export default connect(mapStateToProps,mapActionsToProps)(List);
