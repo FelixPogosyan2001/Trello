c231a37f43974501e3ca8c10a2ae6bcebd4cde5c
Felix <35155993+FelixPogosyan2001@users.noreply.github.com>
2019-06-29 22:29:09 +0300

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,8 +8,8 @@ const Main  = () => {
      <div className = 'App'>
         <div className="row" style = {{margin:'0px'}}>
           <Info/>
-          </div>
-            <img id = 'tr_ph' src = {Trello} style = {{width:'60%',position:'absolute',top:'50%',right:'20%'}} />
+         </div>
+        <img id = 'tr_ph' src = {Trello} style = {{width:'60%',position:'absolute',top:'50%',right:'20%'}} />
      </div>
     )
 }
